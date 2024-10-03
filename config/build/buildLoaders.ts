8d17bdfb3b8c685948ab3feabf3d0727af445fb1
yaroslav <Yaroslav Yanovich>
2024-10-03 17:00:56 +0300

    Add css modules suppot

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,9 +5,18 @@ import { BuildOptions } from "./types/types"
 export const buildLoaders = (options: BuildOptions): ModuleOptions["rules"] => {
   const isDev = options.mode === "development"
 
+  const cssLoaderWithModules = {
+    loader: "css-loader",
+    options: {
+      modules: {
+        localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]"
+      }
+    }
+  }
+
   const sassLoader = {
     test: /\.s[ac]ss$/i,
-    use: [isDev ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+    use: [isDev ? "style-loader" : MiniCssExtractPlugin.loader, cssLoaderWithModules, "sass-loader"]
   }
 
   const tsLoader = {
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
-import "./App.scss"
+import classes from "./App.module.scss"
 
 export const App = () => {
   const [count, setCount] = useState(0)
   return (
     <div>
       <p>Clicked {count}</p>
-      <button onClick={() => setCount((prev) => prev + 1)}>Inc</button>
+      <button className={classes.button} onClick={() => setCount((prev) => prev + 1)}>
+        Inc
+      </button>
     </div>
   )
 }
